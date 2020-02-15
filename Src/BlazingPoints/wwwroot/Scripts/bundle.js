0ea23a3aa1419ddd1cd2817abb5c23228dbbc05d
Greg Trevellick <gtrevellick@googlemail.com>
2020-02-15 07:33:31 +0000

    .

--- Src/BlazingPoints/wwwroot/Scripts/bundle.js
+++ Src/BlazingPoints/wwwroot/Scripts/bundle.js
@@ -19,25 +19,36 @@ var vsoContextTeamName;
 function commonFallback1(arg1, arg2) {
     return "commonFallback|" + arg1 + "|" + arg2;//todo should return a promise!
 }
-function commonFetch1(scenario, requestUrl) {
+function commonFetch1(httpVerb, requestUrl, requestBody) {
     return new Promise((resolve1, reject1) => {
-        var httpVerb = "";
         var jsonResponseData = "";
-        switch (scenario) {
-            case "2":
-                httpVerb = "GET";
-                break;
+
+        if (httpVerb === "GET") {
+            $.ajax({
+                dataType: "json",
+                headers: { "Authorization": authHeader },
+                type: httpVerb,
+                url: requestUrl
+            })
+                .then((responseData) => {
+                    jsonResponseData = JSON.stringify(responseData, null, 4);
+                    resolve1(jsonResponseData);
+                });
         }
-        $.ajax({
-            dataType: "json",
-            headers: { "Authorization": authHeader },
-            type: httpVerb,
-            url: requestUrl
-        })
-            .then((responseData) => {
-                jsonResponseData = JSON.stringify(responseData, null, 4);
-                resolve1(jsonResponseData);
-            });
+        else {
+            $.ajax({
+                contentType: "application/json",
+                data: requestBody,
+                dataType: "json",
+                headers: { "Authorization": authHeader },
+                type: httpVerb,
+                url: requestUrl
+            })
+                .then((responseData) => {
+                    jsonResponseData = JSON.stringify(responseData, null, 4);
+                    resolve1(jsonResponseData);
+                });
+        }
     });
 }
 
@@ -86,8 +97,8 @@ async function handleGetIterationData() {
 async function handleGetWorkItemProcessForProjectData() {
     let wipAdoData;
     try {
-        var requestUrl = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/projects/" + vsoContextProjectName + "?api-version=5.1";
-        wipAdoData = await commonFetch1("2", requestUrl);
+        var requestUrl2 = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/projects/" + vsoContextProjectName + "?api-version=5.1";
+        wipAdoData = await commonFetch1("GET", requestUrl2);
     }
     catch (e) {
         wipAdoData = await commonFallback1();
@@ -96,26 +107,28 @@ async function handleGetWorkItemProcessForProjectData() {
 }
 
 /* 3 WORK ITEM PROCESS DATA 2. */
-function fetchTheWorkItemProcessForProjectData2(projectId2) {
-    return new Promise((resolveAA2, rejectAA2) => {
-        var jsonResponseWorkItemProcessData2 = "";
-        var requestUrlGetProjectId2 = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/projects/" + projectId2 + "/properties?api-version=5.1-preview.1";
-        $.ajax({
-            dataType: "json",
-            headers: { "Authorization": authHeader },
-            type: "GET",
-            url: requestUrlGetProjectId2
-        })
-            .then((responseWorkItemProcessData2) => {
-                jsonResponseWorkItemProcessData2 = JSON.stringify(responseWorkItemProcessData2, null, 4);
-                resolveAA2(jsonResponseWorkItemProcessData2);
-            });
-    });
-}
+//function fetchTheWorkItemProcessForProjectData2(projectId2) {
+//    return new Promise((resolveAA2, rejectAA2) => {
+//        var jsonResponseWorkItemProcessData2 = "";
+//        var requestUrlGetProjectId2 = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/projects/" + projectId2 + "/properties?api-version=5.1-preview.1";
+//        $.ajax({
+//            dataType: "json",
+//            headers: { "Authorization": authHeader },
+//            type: "GET",
+//            url: requestUrlGetProjectId2
+//        })
+//            .then((responseWorkItemProcessData2) => {
+//                jsonResponseWorkItemProcessData2 = JSON.stringify(responseWorkItemProcessData2, null, 4);
+//                resolveAA2(jsonResponseWorkItemProcessData2);
+//            });
+//    });
+//}
 async function handleGetWorkItemProcessForProjectData2(projectId2) {
     let wipAdoData2;
     try {
-        wipAdoData2 = await fetchTheWorkItemProcessForProjectData2(projectId2);
+        //wipAdoData2 = await fetchTheWorkItemProcessForProjectData2(projectId2);
+        var requestUrl = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/projects/" + projectId2 + "/properties?api-version=5.1-preview.1";
+        wipAdoData2 = await commonFetch1("GET", requestUrl);
     }
     catch (e) {
         wipAdoData2 = await commonFallback1(projectId2);
@@ -124,26 +137,28 @@ async function handleGetWorkItemProcessForProjectData2(projectId2) {
 }
 
 /* 4 WORK PROCESSES DATA */
-function fetchTheWorkProcessesData() {
-    return new Promise((resolveAA2, rejectAA2) => {
-        var jsonResponseWorkItemProcessData3 = "";
-        var requestUrlGetProjId3 = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/work/processes?api-version=5.1-preview.2";
-        $.ajax({
-            dataType: "json",
-            headers: { "Authorization": authHeader },
-            type: "GET",
-            url: requestUrlGetProjId3
-        })
-            .then((responseWorkItemProcessData3) => {
-                jsonResponseWorkItemProcessData3 = JSON.stringify(responseWorkItemProcessData3, null, 4);
-                resolveAA2(jsonResponseWorkItemProcessData3);
-            });
-    });
-}
+//function fetchTheWorkProcessesData() {
+//    return new Promise((resolveAA2, rejectAA2) => {
+//        var jsonResponseWorkItemProcessData3 = "";
+//        var requestUrlGetProjId3 = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/work/processes?api-version=5.1-preview.2";
+//        $.ajax({
+//            dataType: "json",
+//            headers: { "Authorization": authHeader },
+//            type: "GET",
+//            url: requestUrlGetProjId3
+//        })
+//            .then((responseWorkItemProcessData3) => {
+//                jsonResponseWorkItemProcessData3 = JSON.stringify(responseWorkItemProcessData3, null, 4);
+//                resolveAA2(jsonResponseWorkItemProcessData3);
+//            });
+//    });
+//}
 async function handleGetWorkProcessesData() {
     let wipAdoData3;
     try {
-        wipAdoData3 = await fetchTheWorkProcessesData();
+        //wipAdoData3 = await fetchTheWorkProcessesData();
+        var requestUrl = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/work/processes?api-version=5.1-preview.2";
+        wipAdoData3 = await commonFetch1("GET", requestUrl);
     }
     catch (e) {
         wipAdoData3 = await commonFallback1();
@@ -152,29 +167,40 @@ async function handleGetWorkProcessesData() {
 }
 
 /* 5 WORK ITEM DATA */
-function fetchTheWorkItemData(sprintDate) {
-    return new Promise((resolveB, rejectB) => {
-        var jsonResponseWorkItemData = "";
-        var requestUrl2 = vsoContextHostUri + vsoContextProjectName + "/_apis/wit/wiql?api-version=5.1";
-        var querySelect = "Select [System.Id] From WorkItems Where [System.TeamProject] = '" + vsoContextProjectName + "' And [System.IterationPath] = @CurrentIteration ASOF '" + sprintDate + "'";
-        $.ajax({
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify({ "query": querySelect }),
-            headers: { "Authorization": authHeader },
-            type: "POST",
-            url: requestUrl2
-        })
-            .then((responseWorkItemData) => {
-                jsonResponseWorkItemData = JSON.stringify(responseWorkItemData, null, 4);
-                resolveB(jsonResponseWorkItemData);
-            });
-    });
-}
+//function fetchTheWorkItemData(sprintDate) {
+//    return new Promise((resolveB, rejectB) => {
+//        var jsonResponseWorkItemData = "";
+//        var requestUrl2 = vsoContextHostUri + vsoContextProjectName + "/_apis/wit/wiql?api-version=5.1";
+//        var querySelect = "Select [System.Id] From WorkItems Where [System.TeamProject] = '" + vsoContextProjectName + "' And [System.IterationPath] = @CurrentIteration ASOF '" + sprintDate + "'";
+//        $.ajax({
+//            contentType: "application/json",
+//            dataType: "json",
+//            data: JSON.stringify({ "query": querySelect }),
+//            headers: { "Authorization": authHeader },
+//            type: "POST",
+//            url: requestUrl2
+//        })
+//            .then((responseWorkItemData) => {
+//                jsonResponseWorkItemData = JSON.stringify(responseWorkItemData, null, 4);
+//                resolveB(jsonResponseWorkItemData);
+//            });
+//    });
+//}
 async function handleGetWorkItemData(sprintDate) {
     let wiAdoData;
     try {
-        wiAdoData = await fetchTheWorkItemData(sprintDate);
+        //wiAdoData = await fetchTheWorkItemData(sprintDate);
+        //////////////////var requestUrl5 = "https://dev.azure.com/" + vsoContextAccountName + "/_apis/projects/" + vsoContextProjectName + "?api-version=5.1";
+        var requestUrl5 = vsoContextHostUri + vsoContextProjectName + "/_apis/wit/wiql?api-version=5.1";
+        var querySelect = "Select [System.Id] From WorkItems Where [System.TeamProject] = '" + vsoContextProjectName + "' And [System.IterationPath] = @CurrentIteration ASOF '" + sprintDate + "'";
+        console.log("3333333");
+        console.log(querySelect);
+        var requestBody222 = JSON.stringify({ "query": querySelect });
+        console.log(requestBody222);
+        var requestBody = "{ \"query\": \"Select [System.Id] From WorkItems Where [System.TeamProject] = '" + vsoContextProjectName + "' And [System.IterationPath] = @CurrentIteration ASOF '" + sprintDate + "'\" }";
+        console.log("3333333");
+        console.log(requestBody);
+        wiAdoData = await commonFetch1("POST", requestUrl5, requestBody);
     }
     catch (e) {
         wiAdoData = await commonFallback1(sprintDate);
@@ -183,29 +209,32 @@ async function handleGetWorkItemData(sprintDate) {
 }
 
 /* 6 WORK ITEM ATTRIBUTES DATA */
-function fetchTheWorkItemAttributesBatchData(workItemIds, sprintDateYMDTHMSMSZ) {
-    return new Promise((resolveC, rejectC) => {
-        var jsonResponseWorkItemAttributesBatchData = "";
-        var requestUrl3 = "https://dev.azure.com/" + vsoContextAccountName + "/" + vsoContextProjectName + "/_apis/wit/workitemsbatch?api-version=5.1";
-        var requestBody = '{ "ids": ' + workItemIds + ' , "asOf": "' + sprintDateYMDTHMSMSZ + '", "fields": ["System.Id", "System.Title", "System.WorkItemType", "Microsoft.VSTS.Scheduling.StoryPoints", "System.State", "Microsoft.VSTS.Scheduling.Effort", "Microsoft.VSTS.Scheduling.OriginalEstimate", "Microsoft.VSTS.Scheduling.Size" ] }';
-        $.ajax({
-            contentType: "application/json",
-            dataType: "json",
-            data: requestBody,
-            headers: { "Authorization": authHeader },
-            type: "POST",
-            url: requestUrl3
-        })
-            .then((responseWorkItemAttributesBatchData) => {
-                jsonResponseWorkItemAttributesBatchData = JSON.stringify(responseWorkItemAttributesBatchData, null, 4);
-                resolveC(jsonResponseWorkItemAttributesBatchData);
-            });
-    });
-}
+//function fetchTheWorkItemAttributesBatchData(workItemIds, sprintDateYMDTHMSMSZ) {
+//    return new Promise((resolveC, rejectC) => {
+//        var jsonResponseWorkItemAttributesBatchData = "";
+//        var requestUrl3 = "https://dev.azure.com/" + vsoContextAccountName + "/" + vsoContextProjectName + "/_apis/wit/workitemsbatch?api-version=5.1";
+//        var requestBody = '{ "ids": ' + workItemIds + ' , "asOf": "' + sprintDateYMDTHMSMSZ + '", "fields": ["System.Id", "System.Title", "System.WorkItemType", "Microsoft.VSTS.Scheduling.StoryPoints", "System.State", "Microsoft.VSTS.Scheduling.Effort", "Microsoft.VSTS.Scheduling.OriginalEstimate", "Microsoft.VSTS.Scheduling.Size" ] }';
+//        $.ajax({
+//            contentType: "application/json",
+//            dataType: "json",
+//            data: requestBody,
+//            headers: { "Authorization": authHeader },
+//            type: "POST",
+//            url: requestUrl3
+//        })
+//            .then((responseWorkItemAttributesBatchData) => {
+//                jsonResponseWorkItemAttributesBatchData = JSON.stringify(responseWorkItemAttributesBatchData, null, 4);
+//                resolveC(jsonResponseWorkItemAttributesBatchData);
+//            });
+//    });
+//}
 async function handleGetWorkItemAttributesBatchData(workItemIds, sprintDateYMDTHMSMSZ) {
     let wiabAdoData;
     try {
-        wiabAdoData = await fetchTheWorkItemAttributesBatchData(workItemIds, sprintDateYMDTHMSMSZ);
+        //wiabAdoData = await fetchTheWorkItemAttributesBatchData(workItemIds, sprintDateYMDTHMSMSZ);
+        var requestUrl = "https://dev.azure.com/" + vsoContextAccountName + "/" + vsoContextProjectName + "/_apis/wit/workitemsbatch?api-version=5.1";
+        var requestBody = '{ "ids": ' + workItemIds + ' , "asOf": "' + sprintDateYMDTHMSMSZ + '", "fields": ["System.Id", "System.Title", "System.WorkItemType", "Microsoft.VSTS.Scheduling.StoryPoints", "System.State", "Microsoft.VSTS.Scheduling.Effort", "Microsoft.VSTS.Scheduling.OriginalEstimate", "Microsoft.VSTS.Scheduling.Size" ] }';
+        wiabAdoData = await commonFetch1("POST", requestUrl, requestBody);
     }
     catch (e) {
         wiabAdoData = await commonFallback1(workItemIds, sprintDateYMDTHMSMSZ);
