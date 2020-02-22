57713dcda62814a94f7f5608707e5e343a200ac0
Greg Trevellick <gtrevellick@googlemail.com>
2020-02-22 17:30:52 +0000

    .

--- Src/VsmpBuilder.bat
+++ Src/VsmpBuilder.bat
@@ -1,9 +1,9 @@
 @echo off
 cls
-dotnet build "C:\_Cgit\AzureDevOpsBlazorRadiator\Src\BlazingPoints.sln" /p:configuration=release
+dotnet build "D:\_Dgit\_MINE_ACTIVE\AzureDevOpsBlazorRadiator\Src\BlazingPoints.sln" /p:configuration=release
 rem pause
 
 rem vsmp_pat not used
-cd "C:\_Cgit\AzureDevOpsBlazorRadiator\Src\BlazingPoints"
+cd "D:\_Dgit\_MINE_ACTIVE\AzureDevOpsBlazorRadiator\Src\BlazingPoints"
 tfx extension create --rev-version --manifest-globs vss-extension.json
 rem manually upload the vsix into vsmp edit screen
